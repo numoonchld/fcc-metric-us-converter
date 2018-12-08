57eca1506d1c65a8691bbfa44fbeef7e971a7654
Glitch (fcc-metric-imp-convertor-nmc) <none>
2018-12-08 17:20:58 +0000

    🎍👁 Updated with Glitch

---
+++ tests/2_functional-tests.js
@@ -0,0 +1,111 @@
+/*
+*
+*
+*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
+*       -----[Keep the tests in the same order!]-----
+*       (if additional are added, keep them at the very end!)
+*/
+
+var chaiHttp = require('chai-http');
+var chai = require('chai');
+var assert = chai.assert;
+var server = require('../server');
+
+chai.use(chaiHttp);
+
+suite('Functional Tests', function() {
+
+  suite('Routing Tests', function() {
+    
+    suite('GET /api/convert => conversion object', function() {
+      
+      test('Convert 10L (valid input)', function(done) {
+       chai.request(server)
+        .get('/api/convert')
+        .query({input: '10L'})
+        .end(function(err, res){
+          console.log(res.status,res.body.initNum,res.body.initUnit,res.body.returnNum, res.body.returnUnit)
+          assert.equal(res.status, 200);
+          assert.equal(res.body.initNum, 10);
+          assert.equal(res.body.initUnit, 'L');
+          assert.approximately(res.body.returnNum, 2.64172, 0.1);
+          assert.equal(res.body.returnUnit, 'gal');
+          done();
+        });
+      });
+      
+      test('Convert 32g (invalid input unit)', function(done) {
+        chai.request(server)
+        .get('/api/convert')
+        .query({input: '32g'})
+        .end(function(err, res){
+          console.log(res.status,res.body.initNum,res.body.initUnit,res.body.returnNum, res.body.returnUnit)
+          
+          assert.equal(res.status, 200);
+          assert.equal(res.body.initNum, 32);
+          assert.equal(res.body.initUnit, 'g');
+          assert.equal(res.body.returnNum, null);
+          assert.equal(res.body.returnUnit, 'invalid unit');
+          done();
+          
+        });
+      });
+      
+      test('Convert 3/7.2/4kg (invalid number)', function(done) {
+        chai.request(server)
+        .get('/api/convert')
+        .query({input: '3/7.2/4Kg'})
+        .end(function(err, res){
+          console.log(res.status,res.body.initNum,res.body.initUnit,res.body.returnNum, res.body.returnUnit)
+          
+          assert.equal(res.status, 200);
+          assert.equal(res.body.initNum, 'invalid number');
+          assert.equal(res.body.initUnit, 'Kg');
+          assert.equal(res.body.returnNum, null);
+          assert.equal(res.body.returnUnit, 'lbs');
+          done();
+        });
+      });  
+      
+      test('Convert 3/7.2/4kilomegagram (invalid number and unit)', function(done) {
+        
+        chai.request(server)
+        .get('/api/convert')
+        .query({input: '3/7.2/4kilomegagram'})
+        .end(function(err, res){
+          console.log(res.status,res.body.initNum,res.body.initUnit,res.body.returnNum, res.body.returnUnit)
+          
+          assert.equal(res.status, 200);
+          assert.equal(res.body.initNum, 'invalid number');
+          assert.equal(res.body.initUnit, 'kilomegagram');
+          assert.equal(res.body.returnNum, null );
+          assert.equal(res.body.returnUnit, 'invalid unit');
+          done();
+        });
+        
+      });
+      
+      test('Convert kg (no number)', function(done) {
+        
+        chai.request(server)
+        .get('/api/convert')
+        .query({input: 'Kg'})
+        .end(function(err, res){
+          console.log(res.status,res.body.initNum,res.body.initUnit,res.body.returnNum, res.body.returnUnit)
+          
+          assert.equal(res.status, 200);
+          assert.equal(res.body.initNum, 1);
+          assert.equal(res.body.initUnit, 'Kg');
+          assert.equal(res.body.returnNum, '2.20462' );
+          assert.equal(res.body.returnUnit, 'lbs');
+          done();
+          
+        });
+        
+      });
+      
+    });
+
+  });
+
+});
